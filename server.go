e03cbb9b7b96e7d575bc86207a93dc92c54af2ef
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2022-08-09 00:43:29 +0430

    Implemented Basic-Auth

---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"os"
+	"web-service-gin/controller"
+	"web-service-gin/middlewares"
+	"web-service-gin/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
